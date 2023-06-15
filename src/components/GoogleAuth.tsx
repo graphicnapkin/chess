f324e848c3b7265f2769d2ba2f968a5946421bca
Jon Carter <graphicnapkin>
2023-06-15 00:56:55 -0400

    forgot to save GoogleAuth button that's no longer in use

--- src/components/GoogleAuth.tsx
+++ src/components/GoogleAuth.tsx
@@ -7,21 +7,18 @@ import {
 } from 'firebase/auth'
 
 function GoogleAuth() {
-    const provider = new GoogleAuthProvider()
-
     const signInWithGoogle = () => {
-        signInWithRedirect(auth, provider)
         getRedirectResult(auth)
             .then((result) => {
                 console.log('result', result)
                 // This gives you a Google Access Token. You can use it to access the Google API.
-                const credential =
-                    GoogleAuthProvider.credentialFromResult(result)
-                const token = credential.accessToken
-                // The signed-in user info.
-                const user = result.user
-                // IdP data available using getAdditionalUserInfo(result)
-                // ...
+                // const credential =
+                //     GoogleAuthProvider.credentialFromResult(result)
+                // const token = credential.accessToken
+                // // The signed-in user info.
+                // const user = result.user
+                // // IdP data available using getAdditionalUserInfo(result)
+                // // ...
             })
             .catch((error) => {
                 console.log('error', error)
